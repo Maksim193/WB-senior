4101573762806b5eae6d56dc63c8f8a1b8b6ec6d
Максим Косников <Mayden1127@yandex.ru>
2024-06-30 13:18:20 +0300

    Initial Commit

---
+++ WB-senior/WB_seniorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WB_seniorApp.swift
+//  WB-senior
+//
+//  Created by Максим Косников on 30.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WB_seniorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
